0839723e47f272584290793df479f1ab68fff35b
dhruvalgolakiya <dhruval@golakiya.com>
2023-03-14 12:45:55 +0530

    working on mobile responsive

--- components/PageSection/FeatureSection.tsx
+++ components/PageSection/FeatureSection.tsx
@@ -3,7 +3,7 @@ import Heading from "../Heading";
 
 export default function FeatureSection() {
   return (
-    <div className="relative py-[6.25rem]">
+    <div className="relative py-[5.5rem] lg:py-[6.25rem]">
       <div className=" container flex flex-col mx-auto text-center  items-center text-[#333] ">
         <Heading
           title="Unlove us, we dare you <3"
@@ -11,8 +11,8 @@ export default function FeatureSection() {
           headingColor="text-[#333]"
           descColor="text-[#333]"
         />
-        <div className="  z-[2] w-[100%] mt-[4.375rem] justify-start flex-wrap  items-stretch   flex">
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[312px] xl:h-[388px]  bg-[#f2e6f4] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[38%] mr-[1.5%] mb-[1.5%]">
+        <div className="  z-[2] w-[100%] mt-[4.375rem] justify-start flex-wrap  items-stretch gap-y-[1.5rem] md:gap-y-[0]   flex flex-col md:flex-row ">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[312px] xl:h-[388px]  bg-[#f2e6f4] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[38%] mr-[1.5%] mb-[1.5%]">
             <div className=" flex justify-end w-full">
               <img
                 src="/features/pause.svg"
@@ -20,7 +20,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 Pause / Cancel
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -28,7 +28,7 @@ export default function FeatureSection() {
               </p>
             </div>
           </div>
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[312px] xl:h-[388px]  bg-[#fae8ce] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[25%] mr-[1.5%] mb-[1.5%]">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[312px] xl:h-[388px]  bg-[#fae8ce] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[25%] mr-[1.5%] mb-[1.5%]">
             <div className=" flex justify-start w-full">
               <img
                 src="/features/faster.svg"
@@ -36,7 +36,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 Faster, Smarter & Better
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -45,7 +45,7 @@ export default function FeatureSection() {
               </p>
             </div>
           </div>
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[312px] xl:h-[388px]  bg-[#e4f5d7] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[34%] mb-[1.5%]">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[312px] xl:h-[388px]  bg-[#e4f5d7] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[34%] mb-[1.5%]">
             <div className=" flex justify-start w-full">
               <img
                 src="/features/money-back.svg"
@@ -53,7 +53,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 100% MoneyBack
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -62,7 +62,7 @@ export default function FeatureSection() {
               </p>
             </div>
           </div>
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[268px] xl:h-[343px]  bg-[#fce3e7] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[26%] mr-[1.5%] mb-[1.5%]">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[268px] xl:h-[343px]  bg-[#fce3e7] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[26%] mr-[1.5%] mb-[1.5%]">
             <div className=" flex justify-start w-full">
               <img
                 src="/features/good-design.svg"
@@ -70,7 +70,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 Insanely Good Designs
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -79,7 +79,7 @@ export default function FeatureSection() {
               </p>
             </div>
           </div>
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[268px] xl:h-[343px]  bg-[#faf8cd] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[40%] mr-[1.5%] mb-[1.5%]">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[268px] xl:h-[343px]  bg-[#faf8cd] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[40%] mr-[1.5%] mb-[1.5%]">
             <div className=" flex justify-end w-full">
               <img
                 src="/features/free-trial.svg"
@@ -87,7 +87,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 7 Days Free Trial
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -96,7 +96,7 @@ export default function FeatureSection() {
               </p>
             </div>
           </div>
-          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[268px] xl:h-[343px]  bg-[#e1f2f1] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[31%]  mb-[1.5%]">
+          <div className="flex justify-between border-[2px] rounded-[20px] border-[#333] h-[215px] md:h-[268px] xl:h-[343px]  bg-[#e1f2f1] px-[1.5rem] flex-col pt-[1.5rem] pb-[2rem] w-[100%]  md:w-[31%]  mb-[1.5%]">
             <div className=" flex justify-end w-full">
               <img
                 src="/features/unlimited-req.svg"
@@ -104,7 +104,7 @@ export default function FeatureSection() {
               />
             </div>
             <div className="text-[#333] text-start">
-              <h4 className=" cooper-md text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[3rem]">
+              <h4 className=" cooper-md text-[1.2rem] lg:text-[1.5rem] xl:text-[2rem] leading-[2rem] font-[400] text-[#333] mt-[2rem] md:mt-[3rem]">
                 Unlimited Requests
               </h4>
               <p className="text-[1rem] xl:text-[1.25rem]  inter-font leading-[1.5rem] mt-[0.7rem] max-w-[19rem]">
@@ -119,15 +119,15 @@ export default function FeatureSection() {
         </button>
         <img
           src="/blue-shape.svg"
-          className="absolute left-[4.5%] top-[16.125rem] z-1  xl:w-auto w-[4.5rem]"
+          className="hidden md:absolute left-[4.5%] top-[16.125rem] z-1  xl:w-auto w-[4.5rem]"
         />
         <img
           src="/gray-shape.svg"
-          className="absolute top-[2.5rem] xl:top-[3.5rem] right-[14%] z-[1] xl:right-[22%] w-[5rem] xl:w-auto"
+          className="hidden md:absolute top-[2.5rem] xl:top-[3.5rem] right-[14%] z-[1] xl:right-[22%] w-[5rem] xl:w-auto"
         />
         <img
           src="/red-shape.svg"
-          className="absolute top-[18.5rem] xl:top-[17.5rem] right-[2%] w-[6.5rem] xl:w-auto"
+          className="hidden md:absolute top-[18.5rem] xl:top-[17.5rem] right-[2%] w-[6.5rem] xl:w-auto"
         />
       </div>
     </div>
--- components/PageSection/WhyChooseUs.tsx
+++ components/PageSection/WhyChooseUs.tsx
@@ -13,7 +13,7 @@ export default function WhyChooseUs() {
         />
         <div className="relative">
           <div className=" mt-[2.625rem] rounded-[20px] bg-[#fbf8b6] xl:px-[9.3rem] lg:py-[4.24rem] py-[3rem]  border-[2px] border-[#333]">
-            <p className="cooper-md  px-[1rem] xsm:px-0  mt-[1.4rem] lg:mt-0 text-[1.3rem] md:text-[1.4rem] lg:text-[1.875rem] leading-[2rem] lg:leading-[2.25rem] text-[#333] font-[400] ">
+            <p className="cooper-md  px-[1rem] esm:px-0  mt-[1.4rem] lg:mt-0 text-[1.3rem] md:text-[1.4rem] lg:text-[1.875rem] leading-[2rem] lg:leading-[2.25rem] text-[#333] font-[400] ">
               When you build your own design team
             </p>
             <div className="flex flex-col md:flex-row px-[1.5rem] items-center mt-[2.5rem] lg:mt-[3.625rem] max-w-[45rem] mx-auto lg:max-w-full pb-[0.7rem] justify-between">
@@ -32,23 +32,26 @@ export default function WhyChooseUs() {
               <img src="/final-cost-logo.svg" className="w-[9rem] lg:w-auto" />
             </div>
           </div>
-          <h4 className="text-[8rem] lg:text-[12rem]  cooper-md left-[50%] top-[6.5%] md:top-[47%] lg:top-[50%] mt-0  md:mt-[1rem] font-[400] vs-class text-[rgba(255,6,6,0.2)] absolute">
+          <h4 className="text-[8rem] lg:text-[12rem]  cooper-md left-[50%] top-[69.5%] md:top-[47%] lg:top-[50%] mt-0  md:mt-[1rem] font-[400] vs-class text-[rgba(255,6,6,0.2)] absolute">
             VS
           </h4>
           <div className="mt-[2.625rem]  rounded-[20px] bg-[#e4feef] xl:px-[9.3rem] lg:py-[4.24rem] py-[3rem]  border-[2px] border-[#333]">
-            <p className="cooper-md  px-[1rem] xsm:px-0  mt-[1.4rem] lg:mt-0 text-[1.3rem] md:text-[1.4rem] lg:text-[1.875rem] leading-[2rem] lg:leading-[2.25rem] text-[#333] font-[400] ">
+            <p className="cooper-md  px-[1rem] esm:px-0  mt-[1.4rem] lg:mt-0 text-[1.3rem] md:text-[1.4rem] lg:text-[1.875rem] leading-[2rem] lg:leading-[2.25rem] text-[#333] font-[400] ">
               When you let BubbleWrap do your designs
             </p>
             <div className="flex flex-col  items-center px-[1.5rem] mt-[2.5rem] lg:mt-[3.375rem] ">
-              <p className="text-[] md:text-[1.5rem] leading-[1.875rem]">
+              <p className="text-[1.2rem] md:text-[1.5rem] leading-[1.8rem] md:leading-[1.875rem]">
                 subscriptions start from just
               </p>
-              <h3 className="text-[3.375rem] inter-font font-[600] leading-[4.125rem]">
+              <h3 className=" text-[2rem] esm:text-[2.7rem] md:text-[3.2rem] lg:text-[3.375rem] inter-font font-[600] leading-[2.6rem] esm:leading-[3.3rem] md:leading-[4rem] lg:leading-[4.125rem]">
                 $1,999/month
               </h3>
-              <button className="button-green mt-[3rem] text-[#161616] flex justify-center items-center leading-[1.15rem] gap-x-[8px] inter-font  text-[1.15rem] border-[2px] font-[500] border-[black] py-[1rem] px-[2.5rem] rounded-[40px]">
+              <button className="button-green mt-[2rem] md:mt-[3rem] text-[#161616] flex items-center leading-[1.15rem]  inter-font text-[1rem] md:text-[1.15rem] border-[2px] font-[500] border-[black] py-[1rem] px-[2.5rem] rounded-[40px]">
                 Get Started
-                <img src="https://uploads-ssl.webflow.com/6352e9f1db028e490078b1df/639add9a6f642843c45770cf_btn-arrow.svg" />
+                <img
+                  src="https://uploads-ssl.webflow.com/6352e9f1db028e490078b1df/639add9a6f642843c45770cf_btn-arrow.svg"
+                  className="w-[17px] ml-[5px]"
+                />
               </button>
             </div>
           </div>
